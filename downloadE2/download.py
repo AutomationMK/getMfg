3b365a0a2eb9a543dbe12378f55f9fa1bde4439b
Max Kranker <maxkranker@protonmail.com>
2025-05-29 12:05:01 -0400

    Fixed the ability to find table rows by using the grid download on the page; added in the ability to filter HS from PLT 1 jobs by using the RESALE product code

--- downloadE2/download.py
+++ downloadE2/download.py
@@ -1,13 +1,15 @@
-from playwright.async_api import Playwright, async_playwright, Error
+import os
+from playwright.async_api import Playwright, async_playwright, Error, Page
 import asyncio
 from .encryptPass import password, user, url, company
 import pandas as pd
 from dataHandle.data import format_data
 
-wait_time = 5000  # 60000 = 1min so units are in milliseconds
+wait_time = 1500  # 60000 = 1min so units are in milliseconds
+download_time = 30000  # 60000 = 1min so units are in milliseconds
 
 
-async def safe_goto(page, url, max_retries=10, delay=0.5):
+async def safe_goto(page: Page, url: str, max_retries=10, delay=0.5):
     """Function to safely go to a specific url using playwright
     This fixes the issue where you may not connect to the address
     on the first try. This will try about 10 times until you can connect"""
@@ -30,7 +32,31 @@ async def safe_goto(page, url, max_retries=10, delay=0.5):
     raise Exception(f"Failed to navigate to {url} after {max_retries} attempts.")
 
 
-async def uncheck(page, locationName):
+async def login_e2(page: Page):
+    """Function to log into E2 and this will use the seperate module encryptPass
+    for checking if a user name and password exists if not then it will
+    prompt the user to create one for their E2 account"""
+    await page.get_by_role("textbox", name="").fill(user())
+    await page.get_by_role("textbox", name="").fill(password())
+    await page.get_by_role("button", name=" LOGIN").click()
+    await page.locator("#company").select_option(company())
+    await page.get_by_role("link", name="Main").click()
+    try:
+        await page.get_by_text("This user is already logged").click(timeout=2000)
+        await page.get_by_role("button", name=" Yes").click()
+    except Error as e:
+        if "Timeout" in str(e):
+            pass
+
+
+async def logout_e2(page: Page):
+    """Function to log out of E2, right now it only works for username max and
+    this needs to be made more general for everyone"""
+    await page.locator("span.user-info.hidden-xs").click()
+    await page.locator("#navbar-container").get_by_text("Logout").click()
+
+
+async def uncheck(page: Page, locationName: str):
     """Function to uncheck a checkbox element on E2
     This function first tries to find the element using the locator function
     if that does not work it will use the get_by_text function"""
@@ -41,35 +67,12 @@ async def uncheck(page, locationName):
         await checkbox.click()
 
 
-async def login_e2(page):
-    """Function to log into E2 and this will use the seperate module encryptPass
-    for checking if a user name and password exists if not then it will
-    prompt the user to create one for their E2 account"""
-    await page.get_by_role("textbox", name="").fill(user())
-    await page.get_by_role("textbox", name="").fill(password())
-    await page.get_by_role("button", name=" LOGIN").click()
-    await page.locator("#company").select_option(company())
-    await page.get_by_role("link", name="Main").click()
-    try:
-        await page.get_by_text("This user is already logged").click(timeout=wait_time)
-        await page.get_by_role("button", name=" Yes").click()
-    except Error as e:
-        if "Timeout" in str(e):
-            pass
-
-
-async def logout_e2(page):
-    """Function to log out of E2, right now it only works for username max and
-    this needs to be made more general for everyone"""
-    await page.locator("span.user-info.hidden-xs").click()
-    await page.locator("#navbar-container").get_by_text("Logout").click()
-
-
-async def getmfg(page):
+async def getmfg(page: Page):
     """Function to take mfg cost info from a job details page"""
     job_number_box = page.locator("#cg_tbJobNumber div")
     part_number_box = page.locator("#cg_tbPartNumber div")
     customer_box = page.locator("#cg_tbCustomer div")
+    product_code_box = page.locator("#cg_tbProductCode")
     mfg_box = page.locator("#cg_curUDCurrency2 div").nth(1)
     plt1_box = page.locator("#cg_tbUDNumber1 div").nth(1)
     plt2_box = page.locator("#cg_tbUDNumber2 div").nth(1)
@@ -82,6 +85,8 @@ async def getmfg(page):
     part_number = await page.evaluate("() => window.getSelection().toString()")
     await customer_box.click(click_count=3)
     customer = await page.evaluate("() => window.getSelection().toString()")
+    await product_code_box.click(click_count=3)
+    product_code = await page.evaluate("() => window.getSelection().toString()")
     await mfg_box.click(click_count=3)
     mfg = await page.evaluate("() => window.getSelection().toString()")
     mfg = float(mfg.replace("$", "").replace(",", ""))
@@ -98,11 +103,14 @@ async def getmfg(page):
     total_qty = total_qty.replace(",", "")
     mfg_total = float(total_qty) * mfg
     plt1_total = 0.0
+    hs_total = 0.0
     plt2_total = 0.0
     plt6_total = 0.0
     plt7_total = 0.0
-    if plt1 != "":
+    if plt1 != "" and "RESALE" not in product_code:
         plt1_total = mfg_total * float(plt1) * 0.01
+    if plt1 != "" and "RESALE" in product_code:
+        hs_total = mfg_total * float(plt1) * 0.01
     if plt2 != "":
         plt2_total = mfg_total * float(plt2) * 0.01
     if plt6 != "":
@@ -113,6 +121,7 @@ async def getmfg(page):
         "Job Number": [job_number],
         "Customer": [customer],
         "Part Number": [part_number],
+        "HS": [hs_total],
         "Plt #1": [plt1_total],
         "Plt #2": [plt2_total],
         "Plt #6": [plt6_total],
@@ -123,9 +132,25 @@ async def getmfg(page):
     return totals
 
 
+async def get_grid_rows(page: Page, locator_id: str, column_name: str) -> pd.Series:
+    """Function to get grid data for the line items of a job inquiry"""
+    download_name = (
+        os.path.abspath(".") + "/assets/temp_data/" + "lineItem-GridExport.xlsx"
+    )
+    try:
+        async with page.expect_download(timeout=download_time) as download_info:
+            await page.locator(locator_id).click()
+        download = await download_info.value
+        await download.save_as(download_name)
+    except Error as e:
+        print(e)
+        raise
+    df = pd.read_excel(download_name, dtype="string")
+    os.remove(download_name)
+    return df[column_name]
+
+
 async def loopThroughLineItems(page):
-    itemIndex = 1
-    stillItems = True
     customer_box = page.locator("#cg_tbCustomer div")
     await customer_box.click(click_count=3)
     customer = await page.evaluate("() => window.getSelection().toString()")
@@ -145,28 +170,29 @@ async def loopThroughLineItems(page):
         "Job Number",
         "Customer",
         "Part Number",
+        "HS",
         "Plt #1",
         "Plt #2",
         "Plt #6",
         "Plt #7",
     ]
     totals = pd.DataFrame(columns=columns)
-    while stillItems:
+    await page.wait_for_selector(".tabulator-row")
+    rows = await get_grid_rows(page, "#btnPrint_gvOrderDetailsGrid", "Job Number")
+    for row_id in range(len(rows)):
         try:
-            await page.get_by_role("gridcell", name=f"{job_number}-0{itemIndex}").click(
-                timeout=wait_time
-            )
+            row_name = rows.iloc[row_id]
+            await page.get_by_role("gridcell", name=row_name).click(timeout=wait_time)
             await page.get_by_role("button", name=" Details").click()
             temp_total = await getmfg(page)
             totals = pd.concat([totals if not totals.empty else None, temp_total])
         except Error as e:
-            if "Timeout" in str(e):
-                if itemIndex == 1:
+            if "timeout" in str(e):
+                if row_id == 1:
                     return pd.DataFrame()
-                stillItems = False
             else:
+                print(e)
                 raise
-        itemIndex += 1
     await page.get_by_role("button", name=" Close").click()
     return totals
 
@@ -178,7 +204,7 @@ async def set_asc_sort(sort_locator):
         sort_value = await sort_locator.get_attribute("aria-sort")
 
 
-async def loopThroughJobs(page):
+async def loopThroughJobs(page: Page):
     """Function to loop through all job status jobs with the date
     requested as user input"""
     mfgDate = input("Please input the MFG$ date : ")
@@ -195,12 +221,13 @@ async def loopThroughJobs(page):
     await page.get_by_role("button", name=" Search").click()
     sort_locator = page.get_by_role("columnheader", name="Order ")
     await set_asc_sort(sort_locator)
-    jobIndex = 1
-    stillJobs = True
+    await page.wait_for_selector(".tabulator-row")
+    rows = await get_grid_rows(page, "#btnPrint_grdResults", "Order")
     columns = [
         "Job Number",
         "Customer",
         "Part Number",
+        "HS",
         "Plt #1",
         "Plt #2",
         "Plt #6",
@@ -208,42 +235,23 @@ async def loopThroughJobs(page):
     ]
     totals = pd.DataFrame(columns=columns)
     temp_totals = pd.DataFrame(columns=columns)
-    while stillJobs:
-        if jobIndex == 1:
-            try:
-                await page.locator(".tabulator-row > div:nth-child(3)").first.click(
-                    timeout=wait_time
-                )
-                await page.get_by_role("button", name=" View").click()
-                temp_totals = await loopThroughLineItems(page)
-                if not temp_totals.empty:
-                    totals = pd.concat(
-                        [totals if not totals.empty else None, temp_totals]
-                    )
-                await set_asc_sort(sort_locator)
-            except Error as e:
-                print(f"{e}: no orders found")
-                await logout_e2(page)
-                raise ValueError
-        else:
-            try:
-                await page.locator(
-                    f".tabulator-table > div:nth-child({jobIndex}) > div:nth-child(3)"
-                ).click(timeout=wait_time)
-                await page.get_by_role("button", name=" View").click()
-                temp_totals = await loopThroughLineItems(page)
-                if not temp_totals.empty:
-                    totals = pd.concat(
-                        [totals if not totals.empty else None, temp_totals]
-                    )
-                await set_asc_sort(sort_locator)
-            except Error as e:
-                if "Timeout" in str(e):
-                    stillJobs = False
-                else:
-                    raise
-        jobIndex += 1
+    for row_id in range(len(rows)):
+        try:
+            row_name = rows.iloc[row_id]
+            await page.get_by_role("gridcell", name=row_name).click(timeout=wait_time)
+            await page.get_by_role("button", name=" View").click()
+            temp_totals = await loopThroughLineItems(page)
+            if not temp_totals.empty:
+                totals = pd.concat([totals if not totals.empty else None, temp_totals])
+            await set_asc_sort(sort_locator)
+        except Error as e:
+            if "Timeout" in str(e):
+                pass
+            else:
+                print(e)
+                raise
     await page.get_by_role("button", name=" Close").click()
+    hs_total = totals["HS"].sum()
     plt1_total = totals["Plt #1"].sum()
     plt2_total = totals["Plt #2"].sum()
     plt6_total = totals["Plt #6"].sum()
@@ -253,6 +261,7 @@ async def loopThroughJobs(page):
         "Job Number": [""],
         "Customer": ["Plant Totals"],
         "Part Number": [grand_total],
+        "HS": [hs_total],
         "Plt #1": [plt1_total],
         "Plt #2": [plt2_total],
         "Plt #6": [plt6_total],
